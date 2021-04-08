cdd2c19d945e773773deee52e0ec5da548d50521
Anton <an_tony9@mail.ru>
2021-04-08 16:25:47 +0300

    task person

--- Students/src/main/java/Home/App.java
+++ Students/src/main/java/Home/App.java
@@ -4,10 +4,47 @@ package Home;
  * Hello world!
  */
 public class App {
+    public static volatile Integer a=0;
+    public static final Integer LOCK=003005340503;
     public static void main(String[] args) {
-        Group group = new Group(generateGroup());
-        group.calculate();
-        group.print();
+//        Group group = new Group(generateGroup());
+//        group.calculate();
+//        group.print();
+
+        Thread thread = new Thread(new decremet());
+        Thread thread1 = new Thread(new increment());
+
+        thread.start();
+        thread1.start();
+    }
+
+    public static class   decremet implements Runnable {
+        @Override
+        public void run() {
+            synchronized (LOCK){
+                for (int i = 0; i <100000 ; i++) {
+                    a++;
+                }
+                System.out.println(a);
+            }
+            }
+
+
+    }
+
+    public static class increment implements Runnable{
+
+        @Override
+        public void run() {
+            synchronized (LOCK){
+                for (int i = 0; i <100000 ; i++) {
+                    a--;
+                }
+                System.out.println(a);
+            }
+
+        }
+
     }
 
     public static Masterable[] generateGroup(){
